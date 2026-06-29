b6740ea8803fb912aeda6490af38916bfff3456a
zeynepucuncuoglu <zeynepucuncuogluu@gmail.com>
2026-06-30 00:06:26 +0300

    feat: implement main function to initialize TargetManager and UdpReceiver for message handling

--- qt-operator/src/main.cpp
+++ qt-operator/src/main.cpp
@@ -1,3 +1,25 @@
-int main(){
-    
+#include "TargetManager.h"
+#include "UdpReceiver.h"
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+int main()
+{
+    TargetManager targetManager;
+
+    UdpReceiver receiver(9000, targetManager);
+
+    receiver.start();
+
+    std::cout << "Receiver is running for 10 seconds...\n";
+
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+
+    receiver.stop();
+
+    std::cout << "Program finished\n";
+
+    return 0;
 }
